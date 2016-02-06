2c07016970682d8f82155b4348ede64257dbd01e
wsizim <welson.sizim@gmail.com>
2016-02-06 15:57:55 -0200

    Reease 1

---
+++ ExemploValidacao/Controllers/PessoaController.cs
@@ -0,0 +1,37 @@
+﻿using ExemploValidacao.Models;
+using System.Web.Mvc;
+
+namespace ExemploValidacao.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+            return View(pessoa) ;
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+                                           // public ActionResult LogInUnico { get; set; }
+    }
+}
---
+++ ExemploValidacao/Models/Pessoa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExemploValidacao.Models
+{
+    public class Pessoa
+    {
+
+        [Required(ErrorMessage = "nome deve ser preenchido")]
+        public string Nome { get; set; }
+
+        [StringLength(50,MinimumLength = 5,ErrorMessage = "A observação deve ter entre 5 e 50 caracteres" )]
+        [Required(ErrorMessage ="Preencha a Observação")]
+        public string Observacao { get; set; }
+
+        [Range(18,50, ErrorMessage = "A Idade da pessoa deve ser entre 18 e 50 anos")]
+        [Required(ErrorMessage = "Preencha a Idade")]
+        public int Idade { get; set; }
+
+        [RegularExpression(@"b[A-Z0-9._%-]+@[A-Z0-9.-]+.[A-Z]{2,4}b", ErrorMessage = "E-mail em formato inválido.")]
+        [Required(ErrorMessage = "Email não informado")]
+        public string Email { get; set; }
+
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Login deve possuir letras e deve ter de 5 a 15 caracteres")]
+        [Required(ErrorMessage = "O Login deve ser preenchido")]
+        public string Login { get; set; }
+
+        [StringLength(16, MinimumLength = 5, ErrorMessage = "A senha deve ter entre 5 e 16 caracteres")]
+        [RegularExpression(@"[a-zA-Z0-9]*", ErrorMessage = "Na senha somente são permitidos caracteres alfanuméricos")]
+        [Required(ErrorMessage= "A senha deve ser informada")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
